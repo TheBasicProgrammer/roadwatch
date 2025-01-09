2ec15b32ca9a4f0609d7a89c55a49cc2bba56169
Ryfiel <ryfielvgonzales@gmail.com>
2025-01-09 16:51:28 +0800

    Deleted unnecessary files

--- app.py
+++ app.py
@@ -142,5 +142,5 @@ def video_feed():
 
 if __name__ == '__main__':
     print("Starting Flask application...")
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-    #app.run(host="0.0.0.0", port=5000)
+    #app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=5000)
